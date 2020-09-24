6380f2b74001b77372add896f1c07ac99f2b60eb
johnednansy2121 <johnednansy@gmail.com>
2020-09-24 17:55:35 +0800

    profile picture in view profile page update

--- src/app/pages/user/profile/view/view.component.ts
+++ src/app/pages/user/profile/view/view.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewProfileComponent implements OnInit {
 
+  name = "John Doe";
+  abbrev: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.abbrev = this.name.split(/\s/).reduce((response,word)=> response+=word.slice(0,1),'').substring(0,2);
   }
 
 }
