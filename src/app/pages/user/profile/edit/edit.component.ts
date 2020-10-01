b42ad9d1bfd6c4b46f8e48cc72f1a2d0ca75f6ff
johnednansy2121 <johnednansy@gmail.com>
2020-10-01 18:02:20 +0800

    View and Edit profile updates

--- src/app/pages/user/profile/edit/edit.component.ts
+++ src/app/pages/user/profile/edit/edit.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
 
+  pageTitle: string = 'Edit Profile';
+  pageSubTitle: string = 'Edit the profile of your company or organization.';
   constructor() { }
 
   ngOnInit(): void {
