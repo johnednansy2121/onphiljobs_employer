bd65c672cad3b5f788d547231cc099017a9cd3de
johnednansy2121 <johnednansy@gmail.com>
2020-10-29 18:06:34 +0800

    created starting files for Craete Profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,6 +36,7 @@ import { HomeDataResolver } from './pages/home/resolver/home.resolver';
 import { EditProfileComponent } from './pages/user/profile/edit/edit.component';
 import { TasksComponent } from './pages/tasks/tasks.component';
 import { SettingsComponent } from './pages/user/settings/settings.component';
+import { CreateComponent as CreateProfileComponent } from './pages/user/profile/create/create.component';
 
 const routes: Routes = [
   {
@@ -120,6 +121,10 @@ const routes: Routes = [
       {
         path: 'user/profile/edit',
         component: EditProfileComponent
+      },
+      {
+        path: 'user/profile/create',
+        component: CreateProfileComponent
       },
       {
         path: 'user/settings',
