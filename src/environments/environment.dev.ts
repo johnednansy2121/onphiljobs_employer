a8efcba2392d896dc211c90d80775854e90449cb
johnednansy2121 <johnednansy@gmail.com>
2020-10-29 16:37:28 +0800

    changes values in environment for API

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  name: 'Development',
-  theme: 'green',
+  name: 'Local',
+  theme: 'orange',
   initial_page: '/blog/news',
-  api_path: 'https://devpartnersapi.fllair.com/',
-  domain: 'devpartners.fllair.com'
+  api_path: 'http://ec2-3-16-168-154.us-east-2.compute.amazonaws.com:3000/',
+  domain: 'http://ec2-3-16-168-154.us-east-2.compute.amazonaws.com'
 };
